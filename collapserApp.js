465df30131b011596c729e41bf253c399e613a0a
Hamza Muric <hamzamuric9@gmail.com>
2022-01-15 02:48:42 +0100

    remove console.log

--- collapserApp.js
+++ collapserApp.js
@@ -3,7 +3,6 @@ const ps = document.querySelectorAll('p')
 for (const p of ps) {
 	let oldText = null
 	p.addEventListener('click', e => {
-		console.log('clicked')
 		if (!oldText) {
 			oldText = p.innerHTML
 			p.innerHTML = '...'
